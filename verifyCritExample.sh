2e36fe88436b4748f1fbf64eee1786ccb2f17893
Tadeas Kucera <tadeas@certora.com>
2023-11-16 07:50:53 +0100

    Add conf file for full spec and verifyCritExample script

---
+++ verifyCritExample.sh
@@ -0,0 +1,12 @@
+SPECNAME="fullSpec.spec"
+
+FILE="Mutations/CritExample_EndCanBeCalledTwice.sol"
+echo $FILE 
+certoraRun  $FILE:EnglishAuction dependencies/DummyERC20A.sol dependencies/DummyERC721A.sol \
+    --verify EnglishAuction:$SPECNAME \
+    --link EnglishAuction:token=DummyERC20A \
+    --link EnglishAuction:nft=DummyERC721A \
+    --parametric_contracts EnglishAuction \
+    --optimistic_loop \
+    --rule_sanity basic \
+    --msg "$SPECNAME - $FILE with fullSpec"
